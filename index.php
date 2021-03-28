ac26837d16f962f324df52d0b17661fffeec9434
nyancat <nyancatda@outlook.com>
2021-03-29 00:20:56 +0800

    增加Keywords和Description，方便搜索引擎索引

--- index.php
+++ index.php
@@ -2,13 +2,15 @@
 //引入控制文件
 include 'controller/config.php';
 ?>
-
 <!doctype html>
 <html lang="en">
 <head>
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    
+
+    <meta name="keywords" content="<?php echo $servername;?>"/>
+    <meta name="description" content="<?php echo $introduced;?>"/>
+
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
     

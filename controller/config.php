8435f3dc53d10c6afd6aee7e91338d4c3cfc6e7f
Cao0220 <65110919+Cao0220@users.noreply.github.com>
2021-04-29 20:26:00 +0800

    修复api超时错误
    修复api超时错误

--- controller/config.php
+++ controller/config.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(0);
 $json = file_get_contents("config.json");
 $jsoncf = json_decode($json);
 $servername = $jsoncf->servername;
@@ -6,18 +7,25 @@ $serverip = $jsoncf->ip;
 $serverport = $jsoncf->port;
 $introduced = $jsoncf->introduced;
 $qqgrouplink = $jsoncf->qqgrouplink;
+$qqadmin = $jsoncf->qqadmin;
 $email = $jsoncf->email;
+$status = "不在线";
 
-$motdpe = file_get_contents("http://motdpe.blackbe.xyz/api.php?ip=".$jsoncf->ip."&port=".$jsoncf->port);
-$motd = json_decode($motdpe);
-$status = $motd->status;//服务器状态
-if ($status == "online"){
-    $status = "在线";
-} else {
+try {
+    ini_set('default_socket_timeout', 1);
+    $motdpe = file_get_contents("http://motdpe.blackbe.xyz/api.php?ip=" . $jsoncf->ip . "&port=" . $jsoncf->port);
+    $motd = json_decode($motdpe);
+    $status = $motd->status; //服务器状态
+    if ($status == "online") {
+        $status = "在线";
+        $version = $motd->version; //服务器游戏版本
+        $online = $motd->online; //服务器在线人数
+        $max = $motd->max; //服务器最大在线人数
+        $delay = $motd->delay; //服务器延迟
+        echo $status;
+    } else {
+        $status = "不在线";
+    }
+} catch (Exception $e) {
     $status = "不在线";
 }
-$version = $motd->version;//服务器游戏版本
-$online = $motd->online;//服务器在线人数
-$max = $motd->max;//服务器最大在线人数
-$delay = $motd->delay;//服务器延迟
-?>
